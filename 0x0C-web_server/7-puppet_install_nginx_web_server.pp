591b2b5eaa07d2e319e1250a75202a899498355b
The-Anomaly <favouradekogbe@gmail.com>
2022-09-29 00:06:48 +0000

    update puppet task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -10,7 +10,7 @@ class { 'nginx':
   package_source => 'nginx-stable',
 }
 
-nginx::resource::server { '3.236.207.57':
+nginx::resource::server { '44.200.141.48':
   listen_port      => 80,
   www_root         => '/var/www/html/',
   vhost_cfg_append => { 'rewrite' => '^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent' },
